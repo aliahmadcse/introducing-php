768e62df20989fab972de43224f4847b45b1c262
aliahmadcse <aliahmadcse@gmail.com>
2019-12-12 15:48:56 +0500

    Adds challenge solution

--- ch02/02_10/embed.php
+++ ch02/02_10/embed.php
@@ -14,15 +14,24 @@ $characters = [
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Challenge: Embed in HTML</title>
     <link href="styles.css" rel="stylesheet" type="text/css">
 </head>
-<body>
-<h1>Book</h1>
-<p>Description</p>
-<h2>Main Characters</h2>
 
+<body>
+    <h1>Book</h1>
+    <p><?= "{$book['title']} by {$book['author']} and 
+    {$book['description']}"; ?> </p>
+    <h2>Main Characters</h2>
+    <ul>
+        <?php for ($index = 0; $index < sizeof($characters); $index++) {
+            echo "<li> $characters[$index]</li>";
+        }
+        ?>
+    </ul>
 </body>
+
 </html>
