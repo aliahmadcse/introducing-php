d2e7c77cebf4951cd705408e509c8679e1e5659c
aliahmadcse <aliahmadcse@gmail.com>
2019-12-13 00:08:36 +0500

    Completes chapter 04

--- ch04/04_05/integer_division.php
+++ ch04/04_05/integer_division.php
@@ -2,6 +2,7 @@
 // Convert $total_minutes to hours and minutes.
 
 $total_minutes = 318;
+$hours=intdiv($total_minutes,60);
 $minutes = $total_minutes % 60;
 
-echo "Time taken was ?? hours $minutes minutes";
+echo "Time taken was $hours hours $minutes minutes";
