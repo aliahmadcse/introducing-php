e31545bc9bffec568d4499dc7e62fe5b1c9f4f29
aliahmadcse <aliahmadcse@gmail.com>
2019-12-16 00:37:22 +0500

    Adds 07_04

--- ch07/07_02/parent.php
+++ ch07/07_02/parent.php
@@ -1,13 +1,21 @@
+<?php
+$site_root = "/introducing-php/ch07/07_02";
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Using server-side includes</title>
     <link href="styles.css" rel="stylesheet" type="text/css">
 </head>
+
 <body>
-<h1>Including External Files</h1>
-<p>This paragraph is in the original file.</p>
-<p>This is also in the original file.</p>
+    <h1>Including External Files</h1>
+    <p>This paragraph is in the original file.</p>
+    <?php include_once "./includes/para.html"; ?>
+    <p>This is also in the original file.</p>
+    <?php include_once "./includes/para.html"; ?>
 </body>
+
 </html>
