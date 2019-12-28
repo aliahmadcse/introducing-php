83f1921d9da32503dbf51fa4311672f64827f2b4
aliahmadcse <aliahmadcse@gmail.com>
2019-12-28 18:18:56 +0500

    Adds 09_04

--- ch09/09_04/begin/contact.php
+++ ch09/09_04/begin/contact.php
@@ -2,52 +2,66 @@
 $errors = [];
 $missing = [];
 if (isset($_POST['send'])) {
-    $expected = ['name', 'email', 'comments'];
-    $required = ['name', 'comments'];
-    require './includes/process_mail.php';
+  $expected = ['name', 'email', 'comments'];
+  $required = ['name', 'comments'];
+  require './includes/process_mail.php';
 }
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<title>Preserving input</title>
-<link href="styles.css" rel="stylesheet" type="text/css">
+  <meta charset="UTF-8">
+  <title>Preserving input</title>
+  <link href="styles.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
-<h1>Contact Us</h1>
-<?php if ($errors || $missing) : ?>
-<p class="warning">Please fix the item(s) indicated</p>
-<?php endif; ?>
-<form method="post" action="<?= $_SERVER['PHP_SELF']; ?>">
-  <p>
-    <label for="name">Name:
-    <?php if ($missing && in_array('name', $missing)) : ?>
-        <span class="warning">Please enter your name</span>
-    <?php endif; ?>
-    </label>
-    <input type="text" name="name" id="name">
-  </p>
-  <p>
-    <label for="email">Email:
+  <h1>Contact Us</h1>
+  <?php if ($errors || $missing) : ?>
+    <p class="warning">Please fix the item(s) indicated</p>
+  <?php endif; ?>
+  <form method="post" action="<?= $_SERVER['PHP_SELF']; ?>">
+    <p>
+      <label for="name">Name:
+        <?php if ($missing && in_array('name', $missing)) : ?>
+          <span class="warning">Please enter your name</span>
+        <?php endif; ?>
+      </label>
+      <input type="text" name="name" id="name" 
+        <?php if ($missing || $errors) {
+          echo 'value="'. htmlentities($name) .'"';
+        } ?> 
+      />
+    </p>
+    <p>
+      <label for="email">Email:
         <?php if ($missing && in_array('email', $missing)) : ?>
-            <span class="warning">Please enter your email address</span>
+          <span class="warning">Please enter your email address</span>
         <?php endif; ?>
-    </label>
-    <input type="email" name="email" id="email">
-  </p>
-  <p>
-    <label for="comments">Comments:
+      </label>
+      <input type="email" name="email" id="email"
+        <?php if ($missing || $errors) {
+          echo 'value="'. htmlentities($email) .'"';
+          } ?> 
+      />
+    </p>
+    <p>
+      <label for="comments">Comments:
         <?php if ($missing && in_array('comments', $missing)) : ?>
-            <span class="warning">You forgot to add any comments</span>
+          <span class="warning">You forgot to add any comments</span>
         <?php endif; ?>
-    </label>
-      <textarea name="comments" id="comments"></textarea>
-  </p>
-  <p>
-    <input type="submit" name="send" id="send" value="Send Comments">
-  </p>
-</form>
+      </label>
+      <textarea name="comments" id="comments"><?php
+        if ($missing || $errors) {
+            echo htmlentities($comments);
+          } 
+      ?></textarea>
+    </p>
+    <p>
+      <input type="submit" name="send" id="send" value="Send Comments">
+    </p>
+  </form>
 </body>
+
 </html>
